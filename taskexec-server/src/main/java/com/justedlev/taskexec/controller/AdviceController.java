0dadf38a85c513ba1fb5f56b594764f73a5c1480
Edward L <59051661+Justedlev@users.noreply.github.com>
2022-12-17 16:26:10 +0200

    -

--- taskexec-server/src/main/java/com/justedlev/taskexec/controller/AdviceController.java
+++ taskexec-server/src/main/java/com/justedlev/taskexec/controller/AdviceController.java
@@ -1,7 +1,9 @@
 package com.justedlev.taskexec.controller;
 
 import com.justedlev.taskexec.model.response.ErrorDetailsResponse;
+import feign.FeignException;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,13 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import javax.persistence.EntityNotFoundException;
 
+@Slf4j
 @ControllerAdvice
 public class AdviceController extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = IllegalArgumentException.class)
     public ResponseEntity<ErrorDetailsResponse> handle(Exception ex, WebRequest request) {
+        log.error(ex.getMessage());
+        ex.printStackTrace();
         ErrorDetailsResponse response = ErrorDetailsResponse.builder()
                 .details(request.getDescription(false))
                 .message(ex.getMessage())
@@ -26,12 +31,28 @@ public class AdviceController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity<ErrorDetailsResponse> handleNotFoundException(Exception ex, WebRequest request) {
+        log.error(ex.getMessage());
+        ex.printStackTrace();
         ErrorDetailsResponse response = ErrorDetailsResponse.builder()
                 .details(request.getDescription(false))
                 .message(ex.getMessage())
                 .build();
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = FeignException.class)
+    public ResponseEntity<ErrorDetailsResponse> handleFeignException(FeignException ex,
+                                                                     WebRequest request) {
+        log.error(ex.getMessage());
+        ex.printStackTrace();
+        var errorDetails = ErrorDetailsResponse.builder()
+                .details(request.getDescription(false))
+                .message(ex.getLocalizedMessage())
+                .build();
+        var status = ex.status() > 0 ? HttpStatus.valueOf(ex.status()) : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ResponseEntity<>(errorDetails, status);
     }
 
     @NonNull
@@ -41,6 +62,8 @@ public class AdviceController extends ResponseEntityExceptionHandler {
                                                              @NonNull HttpHeaders headers,
                                                              @NonNull HttpStatus status,
                                                              WebRequest request) {
+        log.error(ex.getMessage());
+        ex.printStackTrace();
         ErrorDetailsResponse response = ErrorDetailsResponse.builder()
                 .details(request.getDescription(false))
                 .message(ex.getMessage())
