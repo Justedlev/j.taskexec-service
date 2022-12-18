6165cdce4ee8d42b6d16d93206987c382c45db8f
Edward L <59051661+Justedlev@users.noreply.github.com>
2022-12-18 11:48:47 +0200

    -

--- taskexec-server/src/main/java/com/justedlev/taskexec/executor/manager/AbstractTaskExecutorHandler.java
+++ taskexec-server/src/main/java/com/justedlev/taskexec/executor/manager/AbstractTaskExecutorHandler.java
@@ -8,19 +8,19 @@ import lombok.extern.slf4j.Slf4j;
 public abstract class AbstractTaskExecutorHandler implements TaskExecutor {
     @Override
     public TaskResultResponse execute(TaskContext context) {
-        try {
+//        try {
             var start = System.currentTimeMillis();
             var res = this.handle(context);
             log.info("Task {} was executed in {} ms: {}",
                     context.getTaskName(), System.currentTimeMillis() - start, res.getMessage());
 
             return res;
-        } catch (Exception e) {
-            var message = String.format("Failed to execute task %s: %s", context.getTaskName(), e.getMessage());
-            log.error(message);
-
-            throw new IllegalStateException(message);
-        }
+//        } catch (Exception e) {
+//            var message = String.format("Failed to execute task %s: %s", context.getTaskName(), e.getMessage());
+//            log.error(message);
+//
+//            throw new IllegalStateException(message);
+//        }
     }
 
     protected abstract TaskResultResponse handle(TaskContext context);
